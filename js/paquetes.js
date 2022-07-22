62c6d2a4156f46fbe4ff65e6495c89174701e5b3
Samuel García <sammomx@gmail.com>
2022-07-21 21:47:07 -0500

    JavaScript - Desafío entregable 4 : INCORPORANDO LIBRERÍAS

---
+++ js/paquetes.js
@@ -0,0 +1,8 @@
+const paquetes = [
+    {id:01, tipo:"Paquete", nombre:"Transporte Redondo y Acceso", precio: 450, foto: "./media/1.jpg" },
+    {id:02, tipo:"Paquete", nombre:"Transporte Redondo", precio: 200, foto: "./media/2.jpg" },
+    {id:03, tipo:"Paquete", nombre:"Acceso a Six Flags", precio: 300, foto: "./media/3.jpg" },
+    {id:04, tipo:"Paquete", nombre:"Transporte Sencillo", precio: 120, foto: "./media/4.jpg" },
+    {id:05, tipo:"Dulce", nombre:"Refresco", precio: 25, foto: "./media/5.jpg" },
+    {id:06, tipo:"Dulce", nombre:"Sandwich", precio: 25, foto: "./media/6.jpg" },
+];
